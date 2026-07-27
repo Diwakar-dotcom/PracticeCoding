a668674394aa375f54907660923e8340d8790195
Diwakar-dotcom <dky2713@gmail.com>
2026-07-27 09:33:00 +0530

    Array IQ

---
+++ 011_Array/001_IQ.js
@@ -0,0 +1,12 @@
+// Q1. [Easy] What is the output?
+const arr = [1, 2, 3];
+console.log(arr.push(4));
+/*
+A) [1,2,3,4]
+B) 4 : correct answer because after pushing the element as a last element -> it will also returns the pushed element
+C) 3
+D) undefined
+*/
+
+console.log(arr); // output: [ 1, 2, 3, 4 ]
+
---
+++ 011_Array/002_IQ.js
@@ -0,0 +1,2 @@
+// Q2. [Medium] What is the output?
+console.log([9, 1, 2].sort()); // output: [ 1, 2, 9 ]
